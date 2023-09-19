47e3443af603348b21c964a3823c7fb867730855
Macbook Air <kornilov.s2001@yandex.ru>
2023-09-19 12:28:40 +0500

    Update

--- ToDoListSWIFT/Views/ListView.swift
+++ ToDoListSWIFT/Views/ListView.swift
@@ -13,18 +13,26 @@ struct ListView: View {
     
     
     var body: some View {
-        List {
-            ForEach(listViewModel.items) { item in
-                ListRowView(item: item)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            listViewModel.updateItem(item: item)
-                        }
+        ZStack {
+            if listViewModel.items.isEmpty {
+                NoItemsView()
+                    .transition(AnyTransition.opacity.animation(.easeIn))
+            } else {
+                List {
+                    ForEach(listViewModel.items) { item in
+                        ListRowView(item: item)
+                            .onTapGesture {
+                                withAnimation(.linear) {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
                     }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
             }
-            .onDelete(perform: listViewModel.deleteItem)
-            .onMove(perform: listViewModel.moveItem)
         }
+        
         .navigationTitle("Список Дел📝")
         .navigationBarItems(
             leading: EditButton(),
